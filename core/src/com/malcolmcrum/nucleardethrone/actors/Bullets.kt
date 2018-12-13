ef84d752e083e2aea9d6baf4e50d513d13c21510
Malcolm <crummynz@gmail.com>
2018-12-13 20:30:34 +1100

    Show some debug "explosions" and it's clear we are destroying the wrong tile

--- core/src/com/malcolmcrum/nucleardethrone/actors/Bullets.kt
+++ core/src/com/malcolmcrum/nucleardethrone/actors/Bullets.kt
@@ -1,8 +1,10 @@
 package com.malcolmcrum.nucleardethrone.actors
 
+import com.badlogic.gdx.graphics.Color
 import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.g2d.Batch
 import com.badlogic.gdx.graphics.g2d.Sprite
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer
 import com.badlogic.gdx.math.Rectangle
 import com.badlogic.gdx.math.Vector2
 import com.malcolmcrum.nucleardethrone.Collides
@@ -16,9 +18,15 @@ class BulletManager(private val checkCollision: (Collides, Vector2) -> Collision
     val texture = Texture("bullet.png")
 
     private val bullets: MutableList<Bullet> = ArrayList()
+    private val destroyedBullets = mutableListOf<Vector2>()
 
     init {
         EVENTS.register(this)
+        EVENTS.register(object: EventListener<BulletImpact> {
+            override fun handle(event: BulletImpact) {
+                destroyedBullets.add(event.position)
+            }
+        })
     }
 
     override fun handle(event: BulletFired) {
@@ -33,6 +41,20 @@ class BulletManager(private val checkCollision: (Collides, Vector2) -> Collision
         val collidedBullets = bullets.filter { checkCollision(it, it.velocity).collided }
         collidedBullets.forEach { EVENTS.notify(BulletImpact(it.position)) }
         bullets.removeAll(collidedBullets)
+        drawDestroyedBullets(batch)
+    }
+
+    private fun drawDestroyedBullets(batch: Batch) {
+        batch.end()
+        val shapeRenderer = ShapeRenderer()
+        shapeRenderer.begin(ShapeRenderer.ShapeType.Filled)
+        shapeRenderer.projectionMatrix = batch.projectionMatrix
+        shapeRenderer.color = Color.BLUE
+        for (bullet in destroyedBullets) {
+            shapeRenderer.circle(bullet.x, bullet.y, 0.5f)
+        }
+        shapeRenderer.end()
+        batch.begin()
     }
 }
 
